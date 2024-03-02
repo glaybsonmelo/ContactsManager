e0778b4a7cead8dc87f9071612ef216baaefe4dc
YourUsername <gleybsonmelo998@gmail.com>
2024-03-02 18:59:56 -0300

    feat: LoginDTO

---
+++ ContactsManager.Core/DTO/LoginDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManager.Core.DTO
+{
+    public class LoginDTO
+    {
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public LoginDTO(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
